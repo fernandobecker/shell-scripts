a3ba6e7c73f2c359f2ce37a8bf0454b8b7a4f09b
surfzera <surfzera@gmail.com>
2018-02-22 15:20:04 -0300

    Add files via upload
    Primeiro commit dos arquivos para monitoramento de processos com shell script.

---
+++ executa_processo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar georreferenciamento.jar -ci 
---
+++ verifica_processo.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+##################################
+# Script para monitorar processo #
+##################################
+# nome do processo a ser filtrado
+PROCESSO="georreferenciamento.jar"
+# intervalo que sera feita a checagem (em segundos)
+INTERVALO=10
+while true; do
+    # numero de cópias do processo rodando
+    OCORRENCIAS=`ps ax | grep $PROCESSO | grep -v grep| wc -l`
+    if [ $OCORRENCIAS -eq 0 ]; then
+               # Se o numero de processos rodando é 0, executa novamente
+               # a aplicação e notifica a area de TI
+               ./executa_processo.sh $PROCESSO
+     fi
+     # Aguarda o intervalo especificado na variável e executa novamente o script
+     sleep $INTERVALO
+done
+# Fim do Script
