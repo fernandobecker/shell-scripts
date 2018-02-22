1f80a0fd32e47be6bb0852df1939932112e33cb4
surfzera <surfzera@gmail.com>
2018-02-22 15:29:36 -0300

    Update verifica_processo.sh

--- verifica_processo.sh
+++ verifica_processo.sh
@@ -7,14 +7,14 @@ PROCESSO="georreferenciamento.jar"
 # intervalo que sera feita a checagem (em segundos)
 INTERVALO=10
 while true; do
-    # numero de cÛpias do processo rodando
+    # numero de c√≥pias do processo rodando
     OCORRENCIAS=`ps ax | grep $PROCESSO | grep -v grep| wc -l`
     if [ $OCORRENCIAS -eq 0 ]; then
-               # Se o numero de processos rodando È 0, executa novamente
-               # a aplicaÁ„o e notifica a area de TI
+               # Se o numero de processos rodando √© 0, executa novamente
+               # a aplica√ß√£o.
                ./executa_processo.sh $PROCESSO
      fi
-     # Aguarda o intervalo especificado na vari·vel e executa novamente o script
+     # Aguarda o intervalo especificado na vari√°vel e executa novamente o script
      sleep $INTERVALO
 done
 # Fim do Script
